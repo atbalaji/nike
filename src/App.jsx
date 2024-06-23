25d9bea6eed982aac2c49541456663aba1af12fa
atbalaji <18101543+atbalaji@users.noreply.github.com>
2024-06-23 12:16:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section>
+      Hero
+    </section>
+    <section className="">
+      
+    </section>
+    <section className=""></section>
+    <section className=""></section>
+    <section className=""></section>
+    <section className=""></section>
+    <section className=""></section>
+    <section className=""></section>
+  </main>
+)
