bf24575ae5718c7288a50f69555d5c20e2e70015
atbalaji <18101543+atbalaji@users.noreply.github.com>
2024-06-23 19:41:52 +0530

    Completed Basic Nav component and working on Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
+import {
+  Hero,
+  Footer,
+  PopularProducts,
+  CustomerReviews,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality
+} from './sections';
+
+import Nav from './components/Nav';
+
 const App = () => (
   <main className="relative">
-    Nav
-    <section>
-      Hero
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
     </section>
-    <section className="">
-      
+    <section className="padding">
+      <PopularProducts />
     </section>
-    <section className=""></section>
-    <section className=""></section>
-    <section className=""></section>
-    <section className=""></section>
-    <section className=""></section>
-    <section className=""></section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
-)
+);
+
+export default App;
